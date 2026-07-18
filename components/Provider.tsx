18d3f420e94176e489385e0f33aec692ce9bcc30
mabdullah356 <abdullahworld111@gmail.com>
2026-07-18 22:27:02 +0500

    add : clerk provider for auth user

---
+++ components/Provider.tsx
@@ -0,0 +1,7 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  return <ClerkProvider >
+          {children}
+    </ClerkProvider>
+}
