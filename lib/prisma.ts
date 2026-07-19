906c29b68b2343edd5c04878bbcf82d8acbe707b
mabdullah356 <abdullahworld111@gmail.com>
2026-07-19 08:28:24 +0500

    feat: update Prisma configuration and add PostgreSQL adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,8 @@
-import { PrismaClient } from "/app/generated/prisma";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
